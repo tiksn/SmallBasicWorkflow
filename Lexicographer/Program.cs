cf5e33aec16fc38bf8a03065505816931877bcda
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-12-31 00:07:19 +0200

    add compiler as a reference

--- Lexicographer/Program.cs
+++ Lexicographer/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace TIKSN.SmallBasicWorkflow.Lexicographer
+﻿using Microsoft.SmallBasic;
+using System.IO;
+
+namespace TIKSN.SmallBasicWorkflow.Lexicographer
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            using (var reader = File.OpenText(args[0]))
+            {
+                Parser parser = new Parser();
+                parser.Parse(reader);
+
+                var x = parser.SymbolTable;
+            }
         }
     }
 }
